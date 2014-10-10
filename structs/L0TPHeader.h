c6b8d17e53a3525955088957328f9a50f832caac
Jonas Kunze <kunze.jonas@gmail.com>
2014-10-10 17:23:12 +0200

    added L0TpHeader.h

---
+++ structs/L0TPHeader.h
@@ -0,0 +1,27 @@
+/*
+ * L0TPHeader.h
+ *
+ *  Created on: Oct 10, 2014
+ *      Author: Jonas Kunze <kunze.jonas@gmail.com>
+ */
+
+#ifndef L0TPHEADER_H_
+#define L0TPHEADER_H_
+
+#include <stdint.h>
+
+struct L0TpHeader {
+	uint8_t refFineTime;
+	uint8_t dataType;
+	uint16_t primitive[7];
+
+	uint16_t previousTimeStampHight;
+	uint16_t memoryAddress;
+
+	uint8_t l0TriggerType;
+	uint8_t previousl0TriggerType;
+	uint16_t previousTimeStampLow;
+
+}__attribute__ ((__packed__));
+
+#endif /* L0TPHEADER_H_ */
