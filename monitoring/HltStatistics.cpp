1a7f04eb6785ecf5b7949529cde616bfe58f8e16
MBoretto <marco.bore@gmail.com>
2016-10-13 13:05:10 +0000

    introducing HtlStatistics

---
+++ monitoring/HltStatistics.cpp
@@ -0,0 +1,23 @@
+/*
+ * HltStatistics.cpp
+ *
+ *  Created on: Oct 13, 2016
+ *      Author: root
+ */
+
+#include <monitoring/HltStatistics.h>
+
+namespace na62 {
+
+std::atomic<uint64_t> HltStatistics::L1InputEvents_(0);
+
+HltStatistics::HltStatistics() {
+	// TODO Auto-generated constructor stub
+
+}
+
+HltStatistics::~HltStatistics() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace na62 */
---
+++ monitoring/HltStatistics.h
@@ -0,0 +1,39 @@
+/*
+ * HltStatistics.h
+ *
+ *  Created on: Oct 13, 2016
+ *      Author: root
+ */
+
+#ifndef MONITORING_HLTSTATISTICS_H_
+#define MONITORING_HLTSTATISTICS_H_
+
+namespace na62 {
+
+#include <atomic>
+
+
+class HltStatistics {
+public:
+	HltStatistics();
+	virtual ~HltStatistics();
+
+	static inline uint64_t GetL1InputEvents() {
+		return L1InputEvents_;
+	}
+	static inline uint64_t SumL1InputEvents(int amount) {
+		return L1InputEvents_.fetch_add(amount, std::memory_order_relaxed);
+	}
+
+private:
+	static std::atomic<uint64_t> L1InputEvents_;
+
+};
+
+
+
+
+
+} /* namespace na62 */
+
+#endif /* MONITORING_HLTSTATISTICS_H_ */
