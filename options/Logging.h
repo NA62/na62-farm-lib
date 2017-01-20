c5b0625c27ced6f029bfe9192bb125d713b7fc85
mboretto <marco.bore@gmail.com>
2017-01-20 15:39:23 +0100

    ers compile

--- options/Logging.h
+++ options/Logging.h
@@ -3,40 +3,34 @@
 
 #ifdef USE_GLOG
 	#include <glog/logging.h>
-	//#define LOG_INFO LOG(INFO)
-	//#define LOG_ERROR LOG(ERROR)
-	//#define LOG_WARNING LOG(WARNING)
-	//#define ENDL ""
-
 	#define LOG_INFO(message)	LOG(INFO) << message
 	#define LOG_ERROR(message)	LOG(ERROR) << message 
 	#define LOG_WARNING(message)	LOG(WARNING) << message
-
 #elif USE_ERS
-	//#include "FunctionBuf.h"
-	//#define LOG_INFO na62::logger::log_info
-	//#define LOG_ERROR na62::logger::log_err
-	//#define LOG_WARNING na62::logger::log_warn
-	//#define ENDL std::flush
-
-
 	#include <ers/ers.h>
+	#define ERS_INFO( message ) do { \
+	{ \
+		ERS_REPORT_IMPL( ers::info, ers::Message, message, ERS_EMPTY ); \
+	} } while(0)
+
+	#define ERS_WARNING( message ) do { \
+	{ \
+		ERS_REPORT_IMPL( ers::warning, ers::Message, message, ERS_EMPTY ); \
+	} } while(0)
+
+	#define ERS_ERROR( message ) do { \
+	{ \
+		ERS_REPORT_IMPL( ers::error, ers::Message, message, ERS_EMPTY ); \
+	} } while(0)
+
 	#define LOG_INFO(message) 	ERS_DEBUG(3,message)
 	#define LOG_ERROR(message)	ERS_ERROR(message)
 	#define LOG_WARNING(message)	ERS_WARNING(message)
-
 #else
 	#include <iostream>
-	//#define LOG_INFO std::cout
-	//#define LOG_ERROR std::cerr
-	//#define LOG_WARNING std::cerr
-	//#define ENDL std::endl
-
 	#define LOG_INFO(message)	std::cout << message << std::endl
 	#define LOG_ERROR(message)	std::cerr << message << std::endl
 	#define LOG_WARNING(message)	std::cerr << message << std::endl
-
-
 #endif
 
 #endif /* LOGGING_H_ */
