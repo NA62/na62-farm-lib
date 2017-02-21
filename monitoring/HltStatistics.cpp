644c2ea4499baa4c088f250139e2ef88da98bcc3
Angela Romano <angela.romano@cern.ch>
2017-02-21 16:10:35 +0100

    - Add counters for L1 trigger and update statistics

--- monitoring/HltStatistics.cpp
+++ monitoring/HltStatistics.cpp
@@ -6,19 +6,16 @@
  */
 
 #include <monitoring/HltStatistics.h>
-//#include <l1/L1TriggerProcessor.h>
+#include <array>
 
 namespace na62 {
 
-std::atomic<uint64_t> HltStatistics::L1InputEvents_(0);
-std::atomic<uint64_t> HltStatistics::L1PhysicsEvents_(0);
-std::atomic<uint64_t> HltStatistics::L1PhysicsEventsByMultipleMasks_(0);
 std::atomic<uint64_t>* HltStatistics::L1Triggers_ = new std::atomic<uint64_t>[0xFF + 1];
-std::atomic<uint64_t> HltStatistics::L1InputEventsPerBurst_(0);
-
 
 std::map<std::string, std::atomic<uint64_t>> HltStatistics::cumulativeCounters_;
 std::map<std::string, std::atomic<uint64_t>> HltStatistics::cumulativeCountersSnapshot_;
+std::map<std::string, std::array<std::atomic<uint64_t>, 16>> HltStatistics::cumulativeDimensionalCounters_;
+//std::map<std::string, std::atomic<uint64_t>*> HltStatistics::cumulativeDimensionalCounters_;
 
 
 HltStatistics::HltStatistics() {
@@ -26,38 +23,110 @@ HltStatistics::HltStatistics() {
 
 }
 void HltStatistics::initialize() {
+
+	cumulativeCounters_["L1InputEvents_"] = 0;
+
+	cumulativeCounters_["L1SpecialEvents_"] = 0;
+	cumulativeCounters_["L1ControlEvents_"] = 0;
+	cumulativeCounters_["L1PeriodicsEvents_"] = 0;
+	cumulativeCounters_["L1PhysicsEvents_"] = 0;
+	cumulativeCounters_["L1PhysicsEventsByMultipleMasks_"] = 0;
+
+//	cumulativeDimensionalCounters_["L1InputEventsPerMask_"] = new std::atomic<uint64_t>[16];
+
+//	cumulativeDimensionalCounters_["L1AcceptedEventsPerMask_"] = new std::atomic<uint64_t>[16];
+
+	cumulativeCounters_["L1OutputEvents_"] = 0;
+
+	cumulativeCounters_["L1AcceptedEvents_"] = 0;
+	cumulativeCounters_["L1TimeoutEvents_"] = 0;
+	cumulativeCounters_["L1AllDisabledEvents_"] = 0;
+	cumulativeCounters_["L1BypassEvents_"] = 0;
+	cumulativeCounters_["L1FlagAlgoEvents_"] = 0;
+	cumulativeCounters_["L1AutoPassEvents_"] = 0;
+
+
 	for (int i = 0; i != 0xFF + 1; i++) {
 		L1Triggers_[i] = 0;
 	}
+	for (int i = 0; i != 16; ++i) {
+		cumulativeDimensionalCounters_["L1InputEventsPerMask_"][i] = 0;
+		cumulativeDimensionalCounters_["L1AcceptedEventsPerMask_"][i] = 0;
+	}
 }
 
-void HltStatistics::updateL1Statistics(Event* event, uint_fast8_t l1TriggerTypeWord) {
-	HltStatistics::sumL1InputEvents(1);
-	//LOG_ERROR("HLT Updating Statistics: " << HltStatistics::GetL1InputEvents() << " /" <<  L1TriggerProcessor::GetL1InputStats());
-	HltStatistics::sumL1InputEventsPerBurst(1);
-	HltStatistics::sumCounter("CounterL1", 1);
+void HltStatistics::updateL1Statistics(Event* const event, std::array<uint_fast8_t, 16> l1TriggerWords, uint_fast8_t l1Trigger) {
+	HltStatistics::sumCounter("L1InputEvents_", 1);
 
-	if (l1TriggerTypeWord != 0) {
-		HltStatistics::sumL1TriggerStats(1, l1TriggerTypeWord);
+
+	uint_fast16_t l0TrigFlags = event->getTriggerFlags();
+
+	/*
+	 *Special triggers are all counted together
+	 *l0 trigger words (NZS): SOB=0x22, EOB=0x23, PEDESTAL_LKR=0x2d, CALIBRATION_LKR(1,2,3,4)=0x30,31,32,33
+	 *l0 trigger word (ZS): GTK pulsers =0x2c
+	 *Separate treatments in processing due to different requests for zero-suppression in LKr
+	 */
+	if (event->isSpecialTriggerEvent() || event->isPulserGTKTriggerEvent()) {
+		HltStatistics::sumCounter("L1SpecialEvents_", 1);
+	}
+	if (event->isControlTriggerEvent()) {
+		HltStatistics::sumCounter("L1ControlEvents_", 1);
+	}
+	if (event->isPeriodicTriggerEvent()) {
+		HltStatistics::sumCounter("L1PeriodicsEvents_", 1);
+	}
+	if (event->isPhysicsTriggerEvent()) {
+		HltStatistics::sumCounter("L1PhysicsEvents_", 1);
+		if (__builtin_popcount((uint) l0TrigFlags) > 1)
+			HltStatistics::sumCounter("L1PhysicsEventsByMultipleMasks_", 1);
+		for (int i = 0; i != 16; i++) {
+			if (l0TrigFlags & (1 << i)) {
+				LOG_INFO("i " << i << " l0TrigFlags " << std::hex << (uint) l0TrigFlags << std::dec);
+				HltStatistics::sumDimensionalCounter("L1InputEventsPerMask_", i, 1);
+				if(l1TriggerWords[i]){
+					LOG_INFO("i " << i << " l1TrigWrd " << (uint) l1TriggerWords[i]);
+					HltStatistics::sumDimensionalCounter("L1AcceptedEventsPerMask_", i, 1);
+				}
+			}
+		}
+	}
+
+	/*
+	 * l1Trigger 8-bit word contains event based l1 trigger info
+	 * bit 0 = physics verdict OR of all l1 trigger masks
+	 * bit 3 = timeout flag (error in trigger configuration)
+	 * bit 4 = l1 trigger disable on this mask
+	 * bit 5 = bypass event (control, periodics, special triggers)
+	 * bit 6 = at least one algo being processed in flagging
+	 * bit 7 = AutoPass (AP) event (fraction of overall bandwidth)
+	 */
+
+	if (l1Trigger & TRIGGER_L1_PHYSICS) HltStatistics::sumCounter("L1AcceptedEvents_",1);
+	if (l1Trigger & TRIGGER_L1_TIMEOUT) HltStatistics::sumCounter("L1TimeoutEvents_",1);
+	if (l1Trigger & TRIGGER_L1_ALLDISABLED) HltStatistics::sumCounter("L1AllDisabledEvents_",1);
+	if (l1Trigger & TRIGGER_L1_BYPASS) HltStatistics::sumCounter("L1BypassEvents_",1);
+	if (l1Trigger & TRIGGER_L1_FLAGALGO) HltStatistics::sumCounter("L1FlagAlgoEvents_",1);
+	if (l1Trigger & TRIGGER_L1_AUTOPASS) HltStatistics::sumCounter("L1AutoPassEvents_",1);
+
+	if (l1Trigger != 0) {
+		HltStatistics::sumCounter("L1OutputEvents_",1);
+		HltStatistics::sumL1TriggerStats(1, l1Trigger);
 	} else {
 		//event has been reduced or downscaled
 	}
 
-	//This counter should not be incremented if the event has been marked as downscaled
-	if (event->isPhysicsTriggerEvent()) {
-		HltStatistics::sumL1PhysicsStats(1);
-		if (__builtin_popcount((uint) event->getTriggerFlags()) > 1) {
-			HltStatistics::sumL1PhysicsByMultipleMasksStats(1);
-		}
-	}
+//	HltStatistics::printCounter();
+//	HltStatistics::printDimensionalCounter();
+
 }
 
 void HltStatistics::updateL2Statistics(Event* event, uint_fast8_t l2Trigger) {
-	HltStatistics::sumCounter("CounterL2", 1);
+//	HltStatistics::sumCounter("CounterL2", 1);
 }
 
 void HltStatistics::updateStorageStatistics() {
-	HltStatistics::sumCounter("CounterStorage", 1);
+//	HltStatistics::sumCounter("CounterStorage", 1);
 }
 
 HltStatistics::~HltStatistics() {
--- monitoring/HltStatistics.h
+++ monitoring/HltStatistics.h
@@ -11,6 +11,13 @@
 #include <atomic>
 #include <eventBuilding/Event.h>
 
+#define TRIGGER_L1_PHYSICS 0x01
+#define TRIGGER_L1_TIMEOUT 0x08
+#define TRIGGER_L1_ALLDISABLED 0x10
+#define TRIGGER_L1_BYPASS 0x20
+#define TRIGGER_L1_FLAGALGO 0x40
+#define TRIGGER_L1_AUTOPASS 0x80
+
 namespace na62 {
 
 
@@ -19,30 +26,9 @@ public:
 	HltStatistics();
 	virtual ~HltStatistics();
 	static void initialize();
-	static void updateL1Statistics(Event* event, uint_fast8_t l1TriggerTypeWord);
+	static void updateL1Statistics(Event* const event, std::array<uint_fast8_t, 16> l1TriggerWords, uint_fast8_t l1Trigger);
 	static void updateL2Statistics(Event* event, uint_fast8_t l2Trigger);
 	static void updateStorageStatistics();
-
-	static inline uint64_t GetL1InputEvents() {
-		return L1InputEvents_;
-	}
-	static inline uint64_t sumL1InputEvents(int amount) {
-		return L1InputEvents_.fetch_add(amount, std::memory_order_relaxed);
-	}
-
-	static inline uint64_t getL1PhysicsStats() {
-		return L1PhysicsEvents_;
-	}
-	static inline uint64_t sumL1PhysicsStats(int amount) {
-		return L1PhysicsEvents_.fetch_add(amount, std::memory_order_relaxed);
-	}
-
-	static inline uint64_t getL1PhysicsByMultipleMasksStats() {
-		return L1PhysicsEventsByMultipleMasks_;
-	}
-	static inline uint64_t sumL1PhysicsByMultipleMasksStats(int amount) {
-		return L1PhysicsEventsByMultipleMasks_.fetch_add(amount, std::memory_order_relaxed);
-	}
 
 	static inline std::atomic<uint64_t>* getL1TriggerStats() {
 		return L1Triggers_;
@@ -50,16 +36,6 @@ public:
 	static inline std::atomic<uint64_t>* sumL1TriggerStats(int amount, uint_fast8_t l1Trigger) {
 		L1Triggers_[l1Trigger].fetch_add(amount, std::memory_order_relaxed);
 		return L1Triggers_;
-	}
-
-	static inline uint64_t getL1InputEventsPerBurst() {
-		return L1InputEventsPerBurst_;
-	}
-	static inline uint64_t sumL1InputEventsPerBurst(int amount) {
-		return L1InputEventsPerBurst_.fetch_add(amount, std::memory_order_relaxed);
-	}
-	static void resetL1InputEventsPerBurst() {
-		L1InputEventsPerBurst_ = 0;
 	}
 
 	//Functions to manipulate the maps
@@ -89,6 +65,18 @@ public:
 		return cumulativeCounters_[key] - cumulativeCountersSnapshot_[key];
 	}
 
+	static void printCounter(){
+		for (auto const& counter : cumulativeCounters_) {
+			    std::cout << counter.first << " => " << counter.second << '\n';
+		}
+	}
+	static void printDimensionalCounter(){
+		for (auto const& counter : cumulativeDimensionalCounters_) {
+			for(uint i=0; i<16; i++)
+				std::cout << counter.first << " => " << counter.second[i] << '\n';
+		}
+	}
+
 	static std::vector<std::string> extractKeys() {
 		std::vector<std::string> keys;
 		for (auto const& counter : cumulativeCounters_) {
@@ -98,18 +86,16 @@ public:
 	}
 
 private:
-	static std::atomic<uint64_t> L1InputEvents_;
-	static std::atomic<uint64_t> L1PhysicsEvents_;
-	static std::atomic<uint64_t> L1PhysicsEventsByMultipleMasks_;
 	static std::atomic<uint64_t>* L1Triggers_;
-	static std::atomic<uint64_t> L1InputEventsPerBurst_;
 
 	//Map containing counters continuously updated by the farm
 	static std::map<std::string, std::atomic<uint64_t>> cumulativeCounters_;
+//	static std::map<std::string, std::atomic<uint64_t>*> cumulativeDimensionalCounters_;
+	static std::map<std::string, std::array<std::atomic<uint64_t>, 16>> cumulativeDimensionalCounters_;
+
 	//Snapshot of counters at the last eob commands
 	static std::map<std::string, std::atomic<uint64_t>> cumulativeCountersSnapshot_;
 
-	static std::map<std::string, std::array<std::atomic<uint64_t>, 16>> cumulativeDimensionalCounters_;
 
 };
 
