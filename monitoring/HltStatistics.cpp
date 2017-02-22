1ff5b68d22e75c7a2c5e99bfa7e5aaa2a45b2617
Angela Romano <Angela.Romano@cern.ch>
2017-02-22 10:28:35 +0100

    adding multidimensional counters per mask

--- monitoring/HltStatistics.cpp
+++ monitoring/HltStatistics.cpp
@@ -12,59 +12,47 @@ namespace na62 {
 
 std::atomic<uint64_t>* HltStatistics::L1Triggers_ = new std::atomic<uint64_t>[0xFF + 1];
 
-std::map<std::string, std::atomic<uint64_t>> HltStatistics::cumulativeCounters_;
-std::map<std::string, std::atomic<uint64_t>> HltStatistics::cumulativeCountersSnapshot_;
-std::map<std::string, std::array<std::atomic<uint64_t>, 16>> HltStatistics::cumulativeDimensionalCounters_;
-//std::map<std::string, std::atomic<uint64_t>*> HltStatistics::cumulativeDimensionalCounters_;
-
+std::map<std::string, std::atomic<uint64_t>> HltStatistics::counters_;
+std::map<std::string, std::array<std::atomic<uint64_t>, 16>> HltStatistics::dimensionalCounters_;
 
 HltStatistics::HltStatistics() {
 	// TODO Auto-generated constructor stub
-
 }
 void HltStatistics::initialize() {
 
+	counters_["L1InputEvents"] = 0;
 
-//	for (auto& key : extractKeys()) {
-//		cumulativeCounters_[key] = 0;
-//	}
-
-
-
-	cumulativeCounters_["L1InputEvents_"] = 0;
-
-	cumulativeCounters_["L1SpecialEvents_"] = 0;
-	cumulativeCounters_["L1ControlEvents_"] = 0;
-	cumulativeCounters_["L1PeriodicsEvents_"] = 0;
-	cumulativeCounters_["L1PhysicsEvents_"] = 0;
-	cumulativeCounters_["L1PhysicsEventsByMultipleMasks_"] = 0;
+	counters_["L1SpecialEvents"] = 0;
+	counters_["L1ControlEvents"] = 0;
+	counters_["L1PeriodicsEvents"] = 0;
+	counters_["L1PhysicsEvents"] = 0;
+	counters_["L1PhysicsEventsByMultipleMasks"] = 0;
 
 //	cumulativeDimensionalCounters_["L1InputEventsPerMask_"] = new std::atomic<uint64_t>[16];
 
 //	cumulativeDimensionalCounters_["L1AcceptedEventsPerMask_"] = new std::atomic<uint64_t>[16];
 
-	cumulativeCounters_["L1OutputEvents_"] = 0;
+	counters_["L1OutputEvents"] = 0;
 
-	cumulativeCounters_["L1AcceptedEvents_"] = 0;
-	cumulativeCounters_["L1TimeoutEvents_"] = 0;
-	cumulativeCounters_["L1AllDisabledEvents_"] = 0;
-	cumulativeCounters_["L1BypassEvents_"] = 0;
-	cumulativeCounters_["L1FlagAlgoEvents_"] = 0;
-	cumulativeCounters_["L1AutoPassEvents_"] = 0;
+	counters_["L1AcceptedEvents"] = 0;
+	counters_["L1TimeoutEvents"] = 0;
+	counters_["L1AllDisabledEvents"] = 0;
+	counters_["L1BypassEvents"] = 0;
+	counters_["L1FlagAlgoEvents"] = 0;
+	counters_["L1AutoPassEvents"] = 0;
 
 
 	for (int i = 0; i != 0xFF + 1; i++) {
 		L1Triggers_[i] = 0;
 	}
 	for (int i = 0; i != 16; ++i) {
-		cumulativeDimensionalCounters_["L1InputEventsPerMask_"][i] = 0;
-		cumulativeDimensionalCounters_["L1AcceptedEventsPerMask_"][i] = 0;
+		dimensionalCounters_["L1InputEventsPerMask"][i] = 0;
+		dimensionalCounters_["L1AcceptedEventsPerMask"][i] = 0;
 	}
 }
 
 void HltStatistics::updateL1Statistics(Event* const event, uint_fast8_t l1Trigger) {
-	HltStatistics::sumCounter("L1InputEvents_", 1);
-
+	HltStatistics::sumCounter("L1InputEvents", 1);
 
 	uint_fast16_t l0TrigFlags = event->getTriggerFlags();
 
@@ -75,25 +63,25 @@ void HltStatistics::updateL1Statistics(Event* const event, uint_fast8_t l1Trigge
 	 *Separate treatments in processing due to different requests for zero-suppression in LKr
 	 */
 	if (event->isSpecialTriggerEvent() || event->isPulserGTKTriggerEvent()) {
-		HltStatistics::sumCounter("L1SpecialEvents_", 1);
+		HltStatistics::sumCounter("L1SpecialEvents", 1);
 	}
 	if (event->isControlTriggerEvent()) {
-		HltStatistics::sumCounter("L1ControlEvents_", 1);
+		HltStatistics::sumCounter("L1ControlEvents", 1);
 	}
 	if (event->isPeriodicTriggerEvent()) {
-		HltStatistics::sumCounter("L1PeriodicsEvents_", 1);
+		HltStatistics::sumCounter("L1PeriodicsEvents", 1);
 	}
 	if (event->isPhysicsTriggerEvent()) {
-		HltStatistics::sumCounter("L1PhysicsEvents_", 1);
+		HltStatistics::sumCounter("L1PhysicsEvents", 1);
 		if (__builtin_popcount((uint) l0TrigFlags) > 1)
-			HltStatistics::sumCounter("L1PhysicsEventsByMultipleMasks_", 1);
+			HltStatistics::sumCounter("L1PhysicsEventsByMultipleMasks", 1);
 		for (int i = 0; i != 16; i++) {
 			if (l0TrigFlags & (1 << i)) {
 				LOG_INFO("i " << i << " l0TrigFlags " << std::hex << (uint) l0TrigFlags << std::dec);
-				HltStatistics::sumDimensionalCounter("L1InputEventsPerMask_", i, 1);
+				HltStatistics::sumDimensionalCounter("L1InputEventsPerMask", i, 1);
 				if(event->getL1TriggerWord(i)){
 					LOG_INFO("i " << i << " l1TrigWrd " << (uint) event->getL1TriggerWord(i));
-					HltStatistics::sumDimensionalCounter("L1AcceptedEventsPerMask_", i, 1);
+					HltStatistics::sumDimensionalCounter("L1AcceptedEventsPerMask", i, 1);
 				}
 			}
 		}
@@ -109,15 +97,15 @@ void HltStatistics::updateL1Statistics(Event* const event, uint_fast8_t l1Trigge
 	 * bit 7 = AutoPass (AP) event (fraction of overall bandwidth)
 	 */
 
-	if (l1Trigger & TRIGGER_L1_PHYSICS) HltStatistics::sumCounter("L1AcceptedEvents_",1);
-	if (l1Trigger & TRIGGER_L1_TIMEOUT) HltStatistics::sumCounter("L1TimeoutEvents_",1);
-	if (l1Trigger & TRIGGER_L1_ALLDISABLED) HltStatistics::sumCounter("L1AllDisabledEvents_",1);
-	if (l1Trigger & TRIGGER_L1_BYPASS) HltStatistics::sumCounter("L1BypassEvents_",1);
-	if (l1Trigger & TRIGGER_L1_FLAGALGO) HltStatistics::sumCounter("L1FlagAlgoEvents_",1);
-	if (l1Trigger & TRIGGER_L1_AUTOPASS) HltStatistics::sumCounter("L1AutoPassEvents_",1);
+	if (l1Trigger & TRIGGER_L1_PHYSICS) HltStatistics::sumCounter("L1AcceptedEvents",1);
+	if (l1Trigger & TRIGGER_L1_TIMEOUT) HltStatistics::sumCounter("L1TimeoutEvents",1);
+	if (l1Trigger & TRIGGER_L1_ALLDISABLED) HltStatistics::sumCounter("L1AllDisabledEvents",1);
+	if (l1Trigger & TRIGGER_L1_BYPASS) HltStatistics::sumCounter("L1BypassEvents",1);
+	if (l1Trigger & TRIGGER_L1_FLAGALGO) HltStatistics::sumCounter("L1FlagAlgoEvents",1);
+	if (l1Trigger & TRIGGER_L1_AUTOPASS) HltStatistics::sumCounter("L1AutoPassEvents",1);
 
 	if (l1Trigger != 0) {
-		HltStatistics::sumCounter("L1OutputEvents_",1);
+		HltStatistics::sumCounter("L1OutputEvents",1);
 		HltStatistics::sumL1TriggerStats(1, l1Trigger);
 	} else {
 		//event has been reduced or downscaled
--- monitoring/HltStatistics.h
+++ monitoring/HltStatistics.h
@@ -23,6 +23,7 @@ public:
 	static void updateL2Statistics(Event* event, uint_fast8_t l2Trigger);
 	static void updateStorageStatistics();
 
+	//TODO remove
 	static inline std::atomic<uint64_t>* getL1TriggerStats() {
 		return L1Triggers_;
 	}
@@ -33,38 +34,47 @@ public:
 
 	//Functions to manipulate the maps
 	static inline uint64_t sumCounter(std::string key, uint amount) {
-		return cumulativeCounters_[key].fetch_add(amount, std::memory_order_relaxed);
+		return counters_[key].fetch_add(amount, std::memory_order_relaxed);
 	}
 	static inline uint64_t getCounter(std::string key) {
-			return cumulativeCounters_[key];
+			return counters_[key];
 	}
 
-	//Functions to manipulate the maps for multidimensinal counter
+	//Functions to manipulate the maps for multidimensional counter
 	static inline uint64_t sumDimensionalCounter(std::string key, uint array_index, uint amount) {
-		return cumulativeDimensionalCounters_[key][array_index].fetch_add(amount, std::memory_order_relaxed);
+		return dimensionalCounters_[key][array_index].fetch_add(amount, std::memory_order_relaxed);
 	}
 	static inline uint64_t getDimensionalCounter(std::string key, uint array_index) {
-			return cumulativeDimensionalCounters_[key][array_index];
+			return dimensionalCounters_[key][array_index];
 	}
 
-	static void countersSnapshot() {
-		//coping all values in the snapshot map
+	static void countersReset() {
 		for (auto& key: extractKeys()) {
-			cumulativeCountersSnapshot_[key] = getCounter(key);
+			counters_[key] = 0;
 		}
 	}
 
-	static uint64_t getLastBurstCounter(std::string key) {
-		return cumulativeCounters_[key] - cumulativeCountersSnapshot_[key];
+	static uint64_t getRollingCounter(std::string key) {
+		return counters_[key];
+	}
+
+	static std::string serializeDimensionalCounter(std::string key) {
+		std::stringstream serializedConters;
+		for(uint index=0; index<16; index++) {
+			if(dimensionalCounters_[key][index] > 0) {//Zero suppression
+				serializedConters << index << ":" <<dimensionalCounters_[key][index] << ";";
+ 			}
+		}
+		return serializedConters.str();
 	}
 
 	static void printCounter(){
-		for (auto const& counter : cumulativeCounters_) {
+		for (auto const& counter : counters_) {
 			    std::cout << counter.first << " => " << counter.second << '\n';
 		}
 	}
 	static void printDimensionalCounter(){
-		for (auto const& counter : cumulativeDimensionalCounters_) {
+		for (auto const& counter : dimensionalCounters_) {
 			for(uint i=0; i<16; i++)
 				std::cout << counter.first << " => " << counter.second[i] << '\n';
 		}
@@ -72,7 +82,7 @@ public:
 
 	static std::vector<std::string> extractKeys() {
 		std::vector<std::string> keys;
-		for (auto const& counter : cumulativeCounters_) {
+		for (auto const& counter : counters_) {
 			keys.push_back(counter.first);
 		}
 		return keys;
@@ -80,32 +90,19 @@ public:
 
 	static std::vector<std::string> extractDimensionalKeys() {
 		std::vector<std::string> keys;
-		for (auto const& counter : cumulativeDimensionalCounters_) {
+		for (auto const& counter : dimensionalCounters_) {
 			keys.push_back(counter.first);
 		}
 		return keys;
 	}
 
-
-
-
-
 private:
 	static std::atomic<uint64_t>* L1Triggers_;
 
 	//Map containing counters continuously updated by the farm
-	static std::map<std::string, std::atomic<uint64_t>> cumulativeCounters_;
-//	static std::map<std::string, std::atomic<uint64_t>*> cumulativeDimensionalCounters_;
-	static std::map<std::string, std::array<std::atomic<uint64_t>, 16>> cumulativeDimensionalCounters_;
-
-	//Snapshot of counters at the last eob commands
-	static std::map<std::string, std::atomic<uint64_t>> cumulativeCountersSnapshot_;
-
+	static std::map<std::string, std::atomic<uint64_t>> counters_;
+	static std::map<std::string, std::array<std::atomic<uint64_t>, 16>> dimensionalCounters_;
 };
-
-
-
-
 
 } /* namespace na62 */
 
